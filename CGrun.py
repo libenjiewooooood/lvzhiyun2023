4c0b1b6122257e44c3c047a43d5310741e71a9e0
libenjiewooooood <151925710+libenjiewooooood@users.noreply.github.com>
2023-12-01 15:17:47 +0900

    Add files via upload

--- CGrun.py
+++ CGrun.py
@@ -8,9 +8,7 @@ u = 3  # 货车最大载货量
 m = 100  # 最大电容量
 # 订单
 order = pd.DataFrame([['A', 'B', 10],
-                      ['A', 'D', 8],
-                      ['B', 'C', 13],
-                      ['D', 'C', 14]], columns=['start', 'end', 'weight'])
+                      ['C', 'D', 14]], columns=['start', 'end', 'weight'])
 # 货运节点
 location = pd.DataFrame([[0, 0],
                          [1, 1],
@@ -21,3 +19,21 @@ location = pd.DataFrame([[0, 0],
 pcost_f, pcost_g = 2, 1.2  # 单位距离满载, 空载耗能
 
 df, V, F, m_f, G, m_g, L, h_gs, b_vl = data_pre(order, location, pcost_f, pcost_g)
+
+
+print('所有节点V：',V)
+print('所有路段L：',L)
+#print(L.index('BC'))
+#for x in G:
+#    print(L.index(x))
+print('满载路段F：',F)
+print('满载消耗m_f：',m_f)
+print(m_f[L.index('BC')])
+#print(m_f[2])
+print('空载路段G：',G)
+#print(G[1])
+print('空载消耗m_f：',m_g)
+print('流矩阵b_vl',b_vl)
+#print(b_vl.iloc[1,2],b_vl.iloc[2,1])
+print(b_vl.loc['S','SA'],b_vl.loc['S','BS'])
+print(b_vl.loc['S','SA'],b_vl.loc['S','BS'])
